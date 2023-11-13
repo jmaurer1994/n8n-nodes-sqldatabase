8c6ce752b1fa0c6d28a5803f306bdf1c61fc3df6
Joe Maurer <jmaurer@hatboro-horsham.org>
2023-11-13 11:34:06 -0500

    add set table attribute option

--- nodes/SqlDatabase/v1/actions/statement/execute/properties.ts
+++ nodes/SqlDatabase/v1/actions/statement/execute/properties.ts
@@ -1,46 +1,52 @@
 import type { StatementProperties } from '../../Interfaces';
 
 export const statementExecuteProperties: StatementProperties = [
-	{
-		displayName: 'SQL Statement',
-		name: 'sqlStatement',
-		type: 'string',
-		required: true,
-		typeOptions: {
-			rows: 12
-		},
-		displayOptions: {
-			show: {
-				resource: ['statement'],
-				operation: ['execute'],
-			},
-		},
-		default: '',
-	},
-	{
-		displayName: 'Additional Options',
-		name: 'additionalOptions',
-		type: 'collection',
-		placeholder: 'Add Option',
-		default: {},
-		displayOptions: {
-			show: {
-				resource: [
-					'statement',
-				],
-				operation: [
-					'execute',
-				],
-			},
-		},
-    options: [
-			{
-				displayName: 'Group Output',
-				name: 'groupOutput',
-				type: 'boolean',
-				default: false,
+  {
+    displayName: 'SQL Statement',
+    name: 'sqlStatement',
+    type: 'string',
+    required: true,
+    typeOptions: {
+      rows: 12
+    },
+    displayOptions: {
+      show: {
+        resource: ['statement'],
+        operation: ['execute'],
       },
-		],
-	},
-	
+    },
+    default: '',
+  },
+  {
+    displayName: 'Additional Options',
+    name: 'additionalOptions',
+    type: 'collection',
+    placeholder: 'Add Option',
+    default: {},
+    displayOptions: {
+      show: {
+        resource: [
+          'statement',
+        ],
+        operation: [
+          'execute',
+        ],
+      },
+    },
+    options: [
+      {
+        displayName: 'Group Output',
+        name: 'groupOutput',
+        type: 'boolean',
+        default: false,
+      },
+      {
+        displayName: 'Set Table Attribute',
+        name: 'tableAttribute',
+        type: 'string',
+        default: '',
+      },
+    ],
+  },
+
 ];
